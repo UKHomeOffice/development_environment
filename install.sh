208c1fc89b8f445e644ee768d44ff0396b11fd83
Mark Olliver <mark@keao.cloud>
2016-08-23 07:05:46 +0100

    update requirements and set install to use tag

--- install.sh
+++ install.sh
@@ -5,11 +5,18 @@ pip install --upgrade pip setuptools ansible
 
 mkdir -p ~/GIT
 cd ~/GIT
-git clone https://github.com/KEAOSolutions/development_environment.git
-cd dsab_dev_environment
+
+if [ ! -d development_environment ]
+then
+  git clone https://github.com/KEAOSolutions/development_environment.git development_environment
+fi
+
+cd development_environment
+git clean -fxd
+git reset --hard
 git checkout v0.0.1
 ansible-galaxy install -r requirements.yml
 
-ansible-playbook -i hostfile -v -K site.yml
+ansible-playbook -i hostfile -v site.yml
 
 exit
