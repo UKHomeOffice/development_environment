200f2129d7756d406df3491bedebcd1c76b838cf
Mark Olliver <mark@keao.cloud>
2016-08-23 06:55:06 +0100

    bump sub requirement versions

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir -p ~/GIT
 cd ~/GIT
 git clone https://github.com/KEAOSolutions/development_environment.git
 cd dsab_dev_environment
-git checkout develop
+git checkout v0.0.1
 ansible-galaxy install -r requirements.yml
 
 ansible-playbook -i hostfile -v -K site.yml
