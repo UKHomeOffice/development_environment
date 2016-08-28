c5b0cc0ef40e5def29caeda0fa109936029b083e
Mark Olliver <mark@keao.cloud>
2016-08-28 09:29:51 +0200

    fix up install script and make generic's with defaults

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -eux
 
+OS=$(cat /etc/os-release|sed -e 's/"//'|grep ID_LIKE|awk -F '=' '{print $2}'|awk '{print $1}')
+
+if [ ${OS} == "debian" ]
+then
+  apt-get -y install python-pip git libssl-dev
+fi
+
+if [ ${OS} == "rhel" ]
+then
+  yum install -y epel-release 
+  yum install -y git python-pip gcc-c++ openssl-devel python-devel
+fi
+
 pip install --upgrade pip setuptools ansible
 
 mkdir -p ~/GIT
@@ -14,9 +27,9 @@ fi
 cd development_environment
 git clean -fxd
 git reset --hard
-git checkout v0.0.1
+git checkout develop
 ansible-galaxy install -r requirements.yml
 
-ansible-playbook -i hostfile -v site.yml
+#ansible-playbook -i hostfile -v site.yml
 
-exit
+#exit
