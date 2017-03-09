0919ac2de6d8c9fb83e2340dd5f102cf031dc100
Mark Olliver <mark@keao.cloud>
2017-03-09 03:22:21 +0000

    fix typo in chown check

--- pxe_files/firstboot_scripts/postinstall.sh
+++ pxe_files/firstboot_scripts/postinstall.sh
@@ -97,7 +97,7 @@ cat <<EOF > /usr/local/bin/unity-privacy-hook.sh
 #!/bin/bash
 gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
 for USER in \`ls -1 /home\`; do
-  if [ ! i\${USER} == "lost+found" ]
+  if [ ! \${USER} == "lost+found" ]
   then
     chown -R \${USER}:\${USER} /home/\${USER}
   fi
